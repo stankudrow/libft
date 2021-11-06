069cc63199a714a80e9978f1459cd46b8af5671f
Stan <mhorton@student.21-school.ru>
2021-11-06 14:37:55 +0300

    ft_calloc: overflow protection

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,20 +6,26 @@
 /*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 22:24:44 by stanislav         #+#    #+#             */
-/*   Updated: 2021/11/03 22:24:45 by stanislav        ###   ########.fr       */
+/*   Updated: 2021/11/06 14:34:45 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// thanks to @jbelinda and @thera for insights
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*mem;
 	size_t	total;
 
+	if (!nmemb || !size)
+	{
+		nmemb = 1;
+		size = 1;
+	}
+	if (size > __SIZE_MAX__ / nmemb)
+		return (NULL);
 	total = nmemb * size;
-	if (!total)
-		total = 1;
 	mem = malloc(total);
 	if (mem)
 		ft_memset(mem, 0, total);
