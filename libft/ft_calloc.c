d8d560441b5b9baed0d0967054056812cf58030f
Stan <mhorton@student.21-school.ru>
2021-11-09 09:45:31 +0300

    ft_calloc.c: size_t max value

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 22:24:44 by stanislav         #+#    #+#             */
-/*   Updated: 2021/11/06 14:34:45 by stanislav        ###   ########.fr       */
+/*   Updated: 2021/11/09 01:29:00 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		nmemb = 1;
 		size = 1;
 	}
-	if (size > __SIZE_MAX__ / nmemb)
+	if (size > (size_t)-1 / nmemb)
 		return (NULL);
 	total = nmemb * size;
 	mem = malloc(total);
