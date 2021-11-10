4dacbe4fa5cb0f7341d99eb36690c22c66024daa
Stan <mhorton@student.21-school.ru>
2021-11-11 01:44:33 +0300

    strmapi: overflow check

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 22:11:52 by stanislav         #+#    #+#             */
-/*   Updated: 2021/11/03 22:11:53 by stanislav        ###   ########.fr       */
+/*   Updated: 2021/11/11 01:43:16 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char			*str;
+	size_t			slen;
 	unsigned int	i;
 
-	str = (char *)ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	slen = ft_strlen(s);
+	if (slen == (size_t)-1)
+		return (NULL);
+	str = (char *)ft_calloc(slen + 1, sizeof(char));
 	if (str)
 	{
 		i = 0;
