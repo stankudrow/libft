9a735beb327e8e33ff889e24f98b7793c6c8eea6
Stan <mhorton@student.21-school.ru>
2021-11-11 01:50:30 +0300

    strdup: overflow check

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 22:10:54 by stanislav         #+#    #+#             */
-/*   Updated: 2021/11/03 22:10:54 by stanislav        ###   ########.fr       */
+/*   Updated: 2021/11/11 01:47:48 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 char	*ft_strdup(const char *s)
 {
 	void	*str;
-	size_t	len;
+	size_t	slen;
 
-	len = ft_strlen(s);
-	str = ft_calloc(len + 1, sizeof(char));
+	slen = ft_strlen(s);
+	if (slen == (size_t)-1)
+		return (NULL);
+	str = ft_calloc(slen + 1, sizeof(char));
 	if (str)
-		ft_memmove(str, s, len + 1);
+		ft_memmove(str, s, slen + 1);
 	return (str);
 }
 // ft_strlcpy(str, s, len + 1); -> ft_memmove(str, s, len + 1);
